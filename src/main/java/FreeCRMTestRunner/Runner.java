93ef092513191b5a047fc727e03e806a1bdf6876
abhishek.goyal2 <abhi25goel@gmail.com>
2018-09-25 12:53:35 +0530

    added tags and hooks with tags

--- src/main/java/FreeCRMTestRunner/Runner.java
+++ src/main/java/FreeCRMTestRunner/Runner.java
@@ -7,12 +7,14 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "C:\\Users\\abhishek.goyal2\\workspace\\FreeCRMBDDFramework\\src\\main\\java\\Features\\dealsmap.feature"
+		features = "C:\\Users\\abhishek.goyal2\\workspace\\FreeCRMBDDFramework\\src\\main\\java\\Features\\taggedhooks.feature"
 		,glue={"stepDefinition"},
 		format= {"pretty","html:test-output","json:test-output/cucumber.json","junit:junitxml/cucumber.xml"},
 		monochrome=true,
 		dryRun=false,
 		strict=true
+		
+//				tags = {"@End2End,@RegressionTest,@Smoke" }	
 		)
 public class Runner {
 
